4ad887d590d2a9e50bc7e2a04e300aba3741039c
Axel200121 <axelmartinez212001@gmail.com>
2024-03-29 22:01:25 -0600

    creación del esqueleto para el dashboard

--- app-ams-points/src/app/app.routes.ts
+++ app-ams-points/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:DashboardComponent
+    }
+];
--- app-ams-points/src/app/pages/dashboard/dashboard.component.ts
+++ app-ams-points/src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent,HeaderComponent,SidebarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 
 }
