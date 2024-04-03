55fd22b223680f10ba6a3829488493fe1bed85c5
Axel200121 <axelmartinez212001@gmail.com>
2024-04-03 15:27:56 -0600

    agregando router link

--- app-ams-points/src/app/app.routes.ts
+++ app-ams-points/src/app/app.routes.ts
@@ -3,22 +3,27 @@ import { AuthComponent } from './pages/auth/auth.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Error500Component } from './pages/errors/error-500/error-500.component';
 import { Error404Component } from './pages/errors/error-404/error-404.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 export const routes: Routes = [
     {
-        path:'home',
+        path:"home",
         component:DashboardComponent
     },
     {
-        path:'',
+        path:"",
         component:AuthComponent
     },
     {
-        path:'sin-acceso',
+        path:"products",
+        component:ProductsComponent
+    },
+    {
+        path:"sin-acceso",
         component:Error500Component
     },
     {
-        path:'**',
+        path:"**",
         component:Error404Component
     }
 
