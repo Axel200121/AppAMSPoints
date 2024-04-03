5d563db7f7cd859a6f4492b791e968f0b9417a6d
Axel200121 <axelmartinez212001@gmail.com>
2024-04-03 16:20:52 -0600

    Diseño UI de panel de usuarios

--- app-ams-points/src/app/pages/usuarios/usuarios.component.ts
+++ app-ams-points/src/app/pages/usuarios/usuarios.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { BreadcrumbComponent } from '../../components/breadcrumb/breadcrumb.component';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-usuarios',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent,HeaderComponent,FooterComponent,BreadcrumbComponent],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.css'
 })
-export class UsuariosComponent {
+export class UsuariosComponent implements OnInit {
+
+  listBreadcrumb: any[] =  [
+    {
+      route:'home',
+      icon:"fa-house",
+      name:"Dashboard"
+    },
+    {
+      route:'home',
+      icon:"fa-users",
+      name:"Usuarios"
+    },
+  ]
+
+  modalTitle!:string;
+
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+
+  openSaveModalUser(){
+    this.modalTitle = "Agrega"
+  }
+
+  openEditarModalUser(){
+    this.modalTitle="Modifica"
+  }
 
 }
