c9d636dcd13c82af726d2c3b8e71eacf726e1e82
Axel200121 <axelmartinez212001@gmail.com>
2024-04-03 21:01:16 -0600

    Diseño UI de clientes

--- app-ams-points/src/app/pages/clients/clients.component.ts
+++ app-ams-points/src/app/pages/clients/clients.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { BreadcrumbComponent } from '../../components/breadcrumb/breadcrumb.component';
 
 @Component({
   selector: 'app-clients',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent,HeaderComponent,FooterComponent,BreadcrumbComponent],
   templateUrl: './clients.component.html',
   styleUrl: './clients.component.css'
 })
-export class ClientsComponent {
+export class ClientsComponent implements OnInit {
+
+  listBreadcrumb: any[] =  [
+    {
+      route:'home',
+      icon:"fa-house",
+      name:"Dashboard"
+    },
+    {
+      route:'home',
+      icon:"fa-user-group",
+      name:"clientes"
+    },
+  ]
+
+  modalTitle!:string;
+
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+
+  openSaveModalUser(){
+    this.modalTitle = "Agrega"
+  }
+
+  openEditarModalUser(){
+    this.modalTitle="Modifica"
+  }
 
 }
