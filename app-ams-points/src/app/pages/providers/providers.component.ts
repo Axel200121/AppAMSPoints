0c0d27a60ae2d176a768ee5919c64248143b10da
Axel200121 <axelmartinez212001@gmail.com>
2024-04-03 21:29:34 -0600

    Diseño UI de proveedores

--- app-ams-points/src/app/pages/providers/providers.component.ts
+++ app-ams-points/src/app/pages/providers/providers.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { BreadcrumbComponent } from '../../components/breadcrumb/breadcrumb.component';
 
 @Component({
   selector: 'app-providers',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent,HeaderComponent,FooterComponent,BreadcrumbComponent],
   templateUrl: './providers.component.html',
   styleUrl: './providers.component.css'
 })
-export class ProvidersComponent {
+export class ProvidersComponent implements OnInit {
+
+  listBreadcrumb: any[] =  [
+    {
+      route:'home',
+      icon:"fa-house",
+      name:"Dashboard"
+    },
+    {
+      route:'home',
+      icon:"fa-truck-field",
+      name:"Proveedores"
+    },
+  ]
+
+  modalTitle!:string;
+
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+
+  openSaveModalUser(){
+    this.modalTitle = "Agrega"
+  }
+
+  openEditarModalUser(){
+    this.modalTitle="Modifica"
+  }
 
 }
